bf5dd72fe3e955f42eed16b4f78affcc1bab124c
TijaniDevIt <tijanidevit@yahoo.com>
2021-09-13 21:21:16 +0100

    SetUp

--- admin/passages.php
+++ admin/passages.php
@@ -12,7 +12,10 @@ if (($adminQuery->rowCount()) == 0) {
   echo "<h1>  <a href='logout.php'> Something went wrong, please login again</a> </h1>";
 }else{
   $user = $adminQuery->fetch(PDO::FETCH_ASSOC);
-
+if (!isset($_GET['q']) || empty($_GET['q'])) {
+    header("Location: question-bank.php");
+}
+$exam_id = decrypt($_GET['q']);
 ?>
 
 <!DOCTYPE html>
@@ -34,6 +37,8 @@ if (($adminQuery->rowCount()) == 0) {
         <?php } ?>
         <!-- App css -->
 
+        <!-- Custom box css -->
+        <link href="<?php echo ROOT; ?>assets/libs/custombox/custombox.min.css" rel="stylesheet">
           <!-- third party css -->
         <link href="<?php echo ROOT; ?>assets/libs/datatables/dataTables.bootstrap4.css" rel="stylesheet" type="text/css" />
         <link href="<?php echo ROOT; ?>assets/libs/datatables/responsive.bootstrap4.css" rel="stylesheet" type="text/css" />
@@ -80,6 +85,10 @@ if (($adminQuery->rowCount()) == 0) {
  <div class="row">
                     <div class="col-sm-12">
                         <div class="card-box">
+                            <?php if (isset($_SESSION['response'])) {
+                                echo $_SESSION['response'];
+                                unset($_SESSION['response']);
+                            } ?>
                             <div class="row justify-content-center">
                                 <div class="col-sm-6">
                                     <div class="text-center">
@@ -87,38 +96,28 @@ if (($adminQuery->rowCount()) == 0) {
                                         <p class="text-muted"> Nisi praesentium similique totam odio obcaecati, reprehenderit,
                                             dignissimos rem temporibus ea inventore alias! Beatae animi nemo ea
                                             tempora, temporibus laborum facilis ut!</p>                                      
-                                            <a href="new-exam.php" class="btn btn-success waves-effect waves-light mt-1">Add new Course</a>
+                                            <a href="new-question.php?q=<?php echo $_GET['q']; ?>" class="btn btn-success waves-effect waves-light mt-1">Add new Question</a>
                                     </div>
                                 </div><!-- end col -->
                             </div><!-- end row -->
-                             <?php if (isset($_SESSION['response'])) {
-                                echo $_SESSION['response'];
-                                unset($_SESSION['response']);
-                            } ?>
+
                               <div class="row">
                     <div class="col-12">
                         <div class="card-box table-responsive">
                              <?php 
-                              $query = $conn->prepare("SELECT exams.exam_id,exams.status,course.course_name,exams.level_id,exams.studentship_type,exams.total_questions,exams.duration,levels.level_name,exams.exam_id,exams.department_id FROM exams JOIN course ON exams.course_id =course.course_id JOIN levels ON exams.level_id = levels.level_id WHERE exams.active='1' ORDER BY exams.status ASC");
+                              $query = $conn->prepare("SELECT * FROM questions WHERE exam_id='$exam_id' AND active='1' ");
                               $query->execute();
                               if (($query->rowCount()) == 0) {
-                                echo "<h1 class='text-center alert alert-danger'>No record found.</h1>";
+                                echo "<h1 class='text-center alert alert-danger'>No question found.</h1>";
                               }else{
-                             ?>
-                                <h4 class="mt-0 header-title text-center">Question Bank</h4>   
+                             ?>  
                                 <hr/>                         
                                 <table id="responsive-datatable" class="table table-bordered table-bordered dt-responsive norap">
                                     <thead>
 
                                     <tr>
                                         <th></th>
-                                        <th>Course</th>
-                                        <th>Department</th>
-                                        <th>Level</th>
-                                        <th>Studentship</th>
-                                        <th>Questions</th>
-                                        <th>Total Exam Questions</th>
-                                        <th>Duration</th>
+                                        <th>Question</th>
                                         <th>Action</th>
                                     </tr>
                                     </thead>
@@ -128,42 +127,56 @@ if (($adminQuery->rowCount()) == 0) {
                                     while ($row= $query->fetch(PDO::FETCH_ASSOC)) { $i++; ?>
                                         <tr>
                                             <td><?php echo $i; ?></td>
-                                            <td><?php echo $row['course_name']; ?></td>
-                                            <td><?php $department_id = explode(';', $row['department_id']);
-
-                                            for ($j=0; $j < count($department_id)-1 ; $j++) { 
-                                                echo getValue('departments','department_id','department_name',$department_id[$j])."_";
-                                            }
-                                             ?>
-                                                
-                                            </td>
-                                            <td><?php echo $row['level_name']; ?></td>
-                                            <td><?php echo ucfirst(str_replace(';', '<br />', $row['studentship_type'])); ?></td>
-                                            <td><?php echo getRowCount('questions','exam_id',$row['exam_id']); ?></td>
-                                            <td><?php echo $row['total_questions']; ?></td>
-                                            <td><?php echo $row['duration']; ?></td>
+                                            <td><?php echo $row['question_description']; ?></td>
                                             <td>
-                                                <?php if ($row['status'] == 'not active') {?>
-                                                <div class="row mb-2">
-                                                    <a href="passages.php?q=<?php echo encrypt($row['exam_id']); ?>" class="btn btn-primary"><i class="fa fa-eye"></i></a>
-                                                
-                                                </div>
                                                 <div class="row">
-                                                    
-                                                    <a href="actions/start-exam.php?q=<?php echo encrypt($row['exam_id']); ?>" class="btn bg-theme-colored"><i class="fa fa-check"></i></a>
-                                                
-                                                <a href="actions/trash-exam.php?q=<?php echo encrypt($row['exam_id']); ?>" class="btn btn-danger ml-2 trash-button2"><i class="fa fa-trash"></i></a>
+                                                    <a  href="#diagnosis-modal<?php echo $row['question_id']; ?>" data-animation="door" data-plugin="custommodal" data-overlayColor="#36404a" class="btn btn-primary btn-block"><i class="fa fa-eye"></i></a>
+                                                <a href="edit-question.php?q=<?php echo encrypt($row['question_id']); ?>&e=<?php echo $_GET['q'] ?>" class="btn btn-warning btn-block"><i class="fa fa-edit"></i></a>
+                                                <a href="actions/trash-question.php?q=<?php echo encrypt($row['question_id']); ?>&e=<?php echo $_GET['q'] ?>" class="btn btn-danger trash-button2 btn-block"><i class="fa fa-trash"></i></a>
                                                 </div>
-                                            <?php }else{?>
-                                               
-                                                    <a href="passages.php?q=<?php echo encrypt($row['exam_id']); ?>" class="btn btn-primary"><i class="fa fa-eye"></i></a>
-                                                
-                                               
-                                                <a href="actions/trash-exam.php?q=<?php echo encrypt($row['exam_id']); ?>" class="btn btn-danger ml-2 trash-button2"><i class="fa fa-trash"></i></a>
-                                                <?php } ?>
                                             </td>
                                         </tr>
+
+        <!-- Modal -->
+        <div id="diagnosis-modal<?php echo $row['question_id']; ?>" class="modal-demo">
+                <button type="button" class="close" onclick="Custombox.modal.close();">
+                    <span>&times;</span><span class="sr-only">Close</span>
+                </button>
+                <h4 class="custom-modal-title">Question Details</h4>
+                <div class="custom-modal-text">
+                  <p><?php echo $row['question_description']; ?></p>
+                  <hr/>
+                  <h4>Options</h4>
+                  <?php $options = explode('&&', $row['options']);
+                            $k=0;
+                            for ($i=0; $i < count($options) ; $i++) { if ($i >=5 ) break;?>
+                                <?php if (test_input($options[$i]) == '') {
+                                   break;
+                                } ?>
+                               <p><b>Option <?php echo $i+1; ?>  => </b><?php echo $options[$i] ?></p>                                
+                            <?php } ?>
+                            <hr/>
+                            <h4>Answer Type</h4>
+                            <p><?php echo $row['answer_type']; ?></p><hr/>
+                            <h4>Correct Answer</h4>
+                            <?php $answer = explode('&&', $row['answer']);
+                            $k=0;
+                            for ($i=0; $i < count($answer) ; $i++) { if ($i >=5) break;?>
+                                 <?php if (test_input($answer[$i]) == '') {
+                                   break;
+                                } ?>
+                               <p>=> <?php echo $answer[$i] ?></p>                                
+                            <?php } ?>
+                                <hr/>
+                            
+                            <?php if ($row['attachment'] != '0') {?>
+                                <h4>Attachment</h4>
+                            <img src="<?php echo ROOT; ?>uploads/<?php echo $row['attachment']; ?>" style="max-height: 300px;width: auto;margin: auto;" alt="" />
+                        <?php } ?>
+                </div>
+            </div>
                                     <?php } ?>
+
                                     </tbody>
                                 </table>
                             <?php } ?>
@@ -190,6 +203,9 @@ if (($adminQuery->rowCount()) == 0) {
         <?php include "dependencies/footer.php"; ?>
         <!-- Vendor js -->
 <script src="<?php echo ROOT; ?>assets/js/vendor.min.js"></script>
+
+        <!-- Modal-Effect -->
+        <script src="<?php echo ROOT; ?>assets/libs/custombox/custombox.min.js"></script>
  <!-- third party js -->
         <script src="<?php echo ROOT; ?>assets/libs/datatables/jquery.dataTables.min.js"></script>
         <script src="<?php echo ROOT; ?>assets/libs/datatables/dataTables.bootstrap4.js"></script>
